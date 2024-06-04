8efd7d59eb0b246bfc1e4499f430cf2a2fd92e43
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2024-06-05 04:47:17 +0800

    程式碼.py

--- 習題/02/程式碼.py
+++ 習題/02/程式碼.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# 定義城市座標
 citys = [
     (0, 3), (0, 0),
     (0, 2), (0, 1),
@@ -10,13 +9,11 @@ citys = [
     (3, 1), (3, 2)
 ]
 
-# 計算城市之間的距離
 def distance(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-# 計算路徑的長度
 def pathLength(p, citys):
     dist = 0
     plen = len(p)
@@ -24,7 +21,6 @@ def pathLength(p, citys):
         dist += distance(citys[p[i]], citys[p[(i + 1) % plen]])
     return dist
 
-# 定義動態規劃算法函數
 def dynamicProgramming(citys):
     n = len(citys)
     dp = np.full((1 << n, n), float('inf'))
@@ -40,8 +36,5 @@ def dynamicProgramming(citys):
     min_dist = min([dp[(1 << n) - 1][i] + distance(citys[i], citys[0]) for i in range(1, n)])
     return min_dist
 
-# 執行動態規劃算法
 min_distance = dynamicProgramming(citys)
-
-# 輸出結果
 print("Minimized Distance:", min_distance)
