8da135e9ed9f42d696331c192c10498e610b8c45
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2024-06-17 08:06:40 +0800

    Update train.py

--- 習題/07/train.py
+++ 習題/07/train.py
@@ -1,123 +1,86 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import matplotlib.pyplot as plt
-import sys
+from tqdm import tqdm
 
-module = __import__(sys.argv[1])
-Net = module.Net
+# 定義神經網路模型
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 定義卷積層和全連接層
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(64 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-n_epochs = 3
-batch_size_train = 64
-batch_size_test = 1000
-learning_rate = 0.01
-momentum = 0.5
-log_interval = 10
+    def forward(self, x):
+        # 正向傳播過程
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 64 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
-random_seed = 1
-torch.backends.cudnn.enabled = False
-torch.manual_seed(random_seed)
+# 設定隨機種子
+torch.manual_seed(1)
 
-train_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('files/', train=True, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(
-                                 (0.1307,), (0.3081,))
-                             ])),
-  batch_size=batch_size_train, shuffle=True)
+# 載入 MNIST 資料集並進行轉換
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
 
-test_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('files/', train=False, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(
-                                 (0.1307,), (0.3081,))
-                             ])),
-  batch_size=batch_size_test, shuffle=True)
+trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                          shuffle=True, num_workers=2)
 
-examples = enumerate(test_loader)
+testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=1000,
+                                         shuffle=False, num_workers=2)
 
-batch_idx, (example_data, example_targets) = next(examples)
+# 創建模型、優化器和損失函數
+net = Net()
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-'''
-import matplotlib.pyplot as plt
+# 訓練模型
+def train_model(model, criterion, optimizer, trainloader, epochs=5):
+    model.train()
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for data in tqdm(trainloader, desc=f'Training Epoch {epoch+1}/{epochs}'):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        
+        print(f'Epoch {epoch+1}, Loss: {running_loss / len(trainloader)}')
 
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
+# 測試模型
+def test_model(model, testloader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    
+    accuracy = 100.0 * correct / total
+    print(f'Accuracy on test set: {accuracy:.2f}%')
 
-plt.show()
-'''
-
-network = Net()
-optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-                      momentum=momentum)
-
-
-train_losses = []
-train_counter = []
-test_losses = []
-test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
-
-def train(epoch):
-  network.train()
-  for batch_idx, (data, target) in enumerate(train_loader):
-    optimizer.zero_grad()
-    output = network(data)
-    loss = F.nll_loss(F.log_softmax(output, dim=1), target)
-    # 注意: nll_loss 是針對 softmax 的 loss
-    # 所以 network 的最後一層一定要是 softmax, 否則不能用 nll_loss
-    # 參考 https://clay-atlas.com/blog/2019/12/16/machine-learning-notes-nllloss-function/
-    # nll_loss 的 output 是 64*10, target 是 64*1
-    # print('output.shape=', output.shape) # output.shape= torch.Size([64, 10])
-    # print('target.shape=', target.shape) # target.shape= torch.Size([64])
-    # print('target=', target) # target= tensor([2, 0, 0, 7, 2, 5, 8, ...)
-    # sys.exit(1)
-    loss.backward()
-    optimizer.step()
-    if batch_idx % log_interval == 0:
-      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        epoch, batch_idx * len(data), len(train_loader.dataset),
-        100. * batch_idx / len(train_loader), loss.item()))
-      train_losses.append(loss.item())
-      train_counter.append(
-        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
-      torch.save(network.state_dict(), 'results/model.pth')
-      torch.save(optimizer.state_dict(), 'results/optimizer.pth')
-
-def test():
-  network.eval()
-  test_loss = 0
-  correct = 0
-  with torch.no_grad():
-    for data, target in test_loader:
-      output = network(data)
-      test_loss += F.nll_loss(F.log_softmax(output, dim=1), target, size_average=False).item()
-      pred = output.data.max(1, keepdim=True)[1]
-      correct += pred.eq(target.data.view_as(pred)).sum()
-  test_loss /= len(test_loader.dataset)
-  test_losses.append(test_loss)
-  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
-
-test()
-for epoch in range(1, n_epochs + 1):
-  train(epoch)
-  test()
-
-fig = plt.figure()
-plt.plot(train_counter, train_losses, color='blue')
-plt.scatter(test_counter, test_losses, color='red')
-plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-plt.xlabel('number of training examples seen')
-plt.ylabel('negative log likelihood loss')
-plt.show()
+# 執行訓練和測試
+train_model(net, criterion, optimizer, trainloader)
+test_model(net, testloader)
